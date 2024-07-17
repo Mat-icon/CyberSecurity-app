845667140ef65968753d7142ca13c58a829099d5
Matthew Ameh <matthewameh007@gmail.com>
2024-07-17 13:52:03 +0100

    char entities

--- src/app/(components)/About.jsx
+++ src/app/(components)/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
         <div className="text-xl">
           <p className=" leading-9">
             <span className=" font-bold">CyberDefense Nigeria</span> is a
-            pioneering organization dedicated to transforming Nigeria's
+            pioneering organization dedicated to transforming Nigeria&apos;s
             reputation in the global cyberspace and showcasing the contributions
             of Nigerians to global cybersecurity efforts. The organization aims
             to act as a unified platform to initiate and integrate cybersecurity
@@ -27,12 +27,12 @@ const About = () => {
             comprehensive service offerings. Our approach is all-encompassing,
             reaching individuals of all ages, businesses across diverse sectors,
             both profit and nonprofit organizations, and government and private
-            entities. Our mission is to restore and uphold our nation's
+            entities. Our mission is to restore and uphold our nation&apos;s
             reputation, ensuring that the internet serves as a catalyst for
             national development rather than a deterrent. Our Team - We are a
             dedicated team of cybersecurity professionals, fueled by a dream to
             create a secure digital world that reflects the true potential of
-            our nation.Together, we will build a future where cybersecurity is
+            our nation. Together, we will build a future where cybersecurity is
             the foundation of progress, and Nigeria stands tall in the global
             digital arena.
           </p>
